1e2146e1b1d958ecc3cd6b6a5a78df59c46ed58a
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-12-27 22:13:32 +0800

    簡化main函式

--- baha_blacklist/main.py
+++ baha_blacklist/main.py
@@ -1,7 +1,6 @@
 import http.cookiejar as cookiejar
 import logging
 import random
-import sys
 import time
 from argparse import Namespace
 from dataclasses import dataclass
@@ -348,10 +347,7 @@ class GamerAPIExtended(GamerAPI):
         return csrf_token
 
 
-def main(args: Namespace, config_name: str = "config.json") -> int:
-    if args.username == "your user name here":
-        raise ValueError("帳號錯誤，請使用 -u 參數設定帳號名稱或到 config.json 修改預設值")
-
+def init_app(args: Namespace, config_name: str = "config.json") -> tuple[Config, GamerAPIExtended]:
     loglevel = logging.INFO
     if args.verbose:
         loglevel = logging.DEBUG
@@ -362,43 +358,60 @@ def main(args: Namespace, config_name: str = "config.json") -> int:
     json_path = str(Path(__file__).parents[1] / config_name)
     config_loader = ConfigLoader(Config())
     config = config_loader.load_config(json_path, args)
+
+    if config.username == "your user name here":
+        raise ValueError("帳號錯誤，請使用 -u 參數設定帳號名稱或到 config.json 修改預設值")
+
     api = GamerAPIExtended(config)
+    return config, api
 
-    try:
-        if not api.login_success():
-            logger.error("登入失敗，請更新 Cookies")
-            sys.exit(0)
 
-        if "export" in args.mode:
-            logger.info("開始匯出黑名單...")
-            existing_users = api.export_users()
-            write_users(config.blacklist_dest, existing_users)
+def real_main(args: Namespace, config: Config, api: GamerAPIExtended) -> int:
+    if not api.login_success():
+        logger.error("登入失敗，請更新 Cookies")
+        return 1
+
+    if "export" in args.mode:
+        logger.info("開始匯出黑名單...")
+        existing_users = api.export_users()
+        write_users(config.blacklist_dest, existing_users)
+    else:
+        existing_users = api.export_users()
+
+    time.sleep(config.min_sleep)
+
+    if "update" in args.mode:
+        logger.info("開始更新黑名單...")
+        try:
+            uids = load_users(config.blacklist_src, api.session)
+        except RequestException as e:
+            logger.error(f"黑名單來源讀取失敗: {e}")
+            uids = []
+        if uids:
+            api.add_users(uids, existing_users, category="bad")
         else:
-            existing_users = api.export_users()
+            logger.info("沒有更新黑名單，因為載入失敗或來源黑名單為空")
 
-        time.sleep(config.min_sleep)
+    if "clean" in args.mode:
+        logger.info("開始清理黑名單...")
+        if args.force_clean or len(existing_users) > config.friend_num:
+            api.batch_auto_remove_user(
+                existing_users, min_visits=config.min_visit, min_days=config.min_day
+            )
+        else:
+            logger.info(f"黑名單數量未超過 {config.friend_num} 人, 跳過自動清理功能")
 
-        if "update" in args.mode:
-            logger.info("開始更新黑名單...")
-            try:
-                uids = load_users(config.blacklist_src, api.session)
-            except RequestException as e:
-                logger.error(f"黑名單來源讀取失敗: {e}")
-                uids = []
-            if uids:
-                api.add_users(uids, existing_users, category="bad")
-            else:
-                logger.info("沒有更新黑名單，因為載入失敗或來源黑名單為空")
+    return 0
 
-        if "clean" in args.mode:
-            logger.info("開始清理黑名單...")
-            if args.force_clean or len(existing_users) > config.friend_num:
-                api.batch_auto_remove_user(
-                    existing_users, min_visits=config.min_visit, min_days=config.min_day
-                )
-            else:
-                logger.info(f"黑名單數量未超過 {config.friend_num} 人, 跳過自動清理功能")
 
-        return 0
-    except Exception as error:
-        raise RuntimeError(f"Runtime error: {error!s}")
+def main(args: Namespace, config_name: str = "config.json") -> int:
+    try:
+        config, api = init_app(args, config_name)
+        return real_main(args, config, api)
+    except ValueError as e:
+        logger.error(f"輸入錯誤: {e}")
+    except RuntimeError as e:
+        logger.error(f"執行階段錯誤: {e}")
+    except Exception as e:
+        logger.exception(f"發生未預期的錯誤: {e}")
+    return 1
